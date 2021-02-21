4dd08b9ed3fe0f0dcf2806cfeb95c02e33e9d4b7
Brian Cochran <brianco1998@gmail.com>
2021-02-21 01:22:15 -0500

    fleshes out tcp server

--- evgp_rcs/tcpserver.py
+++ evgp_rcs/tcpserver.py
@@ -1,10 +1,14 @@
 from PyQt5.QtCore import QObject, QThread, pyqtSignal
 import socket
 import select
+import re
 from race import RaceState
 
 
 class TcpServer(QObject):
+
+    START_CHAR = '$'
+    END_CHAR = ';'
 
     new_connection = pyqtSignal(str) #ip as string
     lost_connection = pyqtSignal(str) #ip as string
@@ -14,22 +18,29 @@ class TcpServer(QObject):
         self.server_port = server_port
         self.server_backlog = server_backlog
         self.continue_run = True
-        self.race_state = RaceState.IN_GARAGE
         self.connections = []
+        self.states = {}
 
 
     def stop(self):
         self.continue_run = False
 
     @QtCore.pyqtSlot(RaceState)
-    def on_race_state_change(self, newState):
-        self.race_state = newState
+    def on_race_state_change(self, ip, newState): #TODO: check speed of handling a "go" as individual events
+        self.states[ip] = newState
 
     def start_server(self):
         self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.server.setblocking(0)
         self.server.bind((socket.gethostname(), self.server_port))
         self.server.listen(self.server_backlog)
+
+    def close_server(self):
+        for s in self.connections:
+            s.shutdown()
+            s.close()
+        s = []
+        self.server.close()
 
     def run_server(self):
         while self.continue_run:
@@ -38,6 +49,7 @@ class TcpServer(QObject):
                 inputs, self.connections, inputs)
             for s in readable:
                 if s is self.server:
+                    #NEW CONNECTION
                     connection, client_address = s.accept()
                     connection.setblocking(0)
                     self.connections.append(connection)
@@ -46,12 +58,42 @@ class TcpServer(QObject):
                 else:
                     data = s.recv(1024)
                     if data:
-                        pass
-                        #TODO: process
+                        addr = s.getpeername()
+                        self.process_message(addr, msg)
                     else:
+                        #LOST A CONNECTION
                         self.connection.remove(s)
+                        addr = s.getpeername()
+                        s.shutdown()
                         s.close()
-                        self.race.removeRacer()
+                        del self.states[addr]
+                        lost_connection.emit(addr)
+                        print(f"Server lost connection to {addr}")
+
             for s in writable:
                 #TODO: some time processing so we don't send repeat signal too fast
-                pass
+                addr = s.getpeername()
+                print(addr) #TODO: rm debug
+                state = self.states[addr]
+                msg = START_CHAR + str(state) + END_CHAR
+                s.send(msg)
+            #TODO: handle exceptional?
+        self.close_server()
+
+
+    def process_message(self, ip, msg):
+        #TODO: handle if a full message doesn't come at once ("unlikely")
+        #TODO: handle multiple messages at once -- I think just grab the last?
+        matches = re.findall('\$([^\$\s]+?);', a) #splits "$something;"" to "something"
+
+        if matches: #TODO: report error if effectively empty message?
+            last_msg = matches[-1]
+            try:
+                new_state = RaceState(last_msg)
+                last_state = self.states[ip]
+                if new_state != last_state: #only report changes to RCSModel
+                    new_response.emit(ip, new_state)
+                    self.states[ip] = new_state
+            except ValueError as e:
+                print(e) #TODO: logging of oddity
+                return
